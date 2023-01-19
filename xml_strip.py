8208c280ffc49edbe500897fbe43f5f605319578
Witherking25 <witherking@withertech.com>
2023-01-18 23:48:53 -0500

    Update xml_strip.py

--- xml_strip.py
+++ xml_strip.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 
 useless_deps=["darwin"]
-with open('/home/ci/.repo/manifest.xml') as oldfile, open('/home/ci/.repo/manifest_new.xml', 'w') as newfile:
+with open(f'{str(Path.home())}/.repo/manifest.xml') as oldfile, open(f'{str(Path.home())}/.repo/manifest_new.xml', 'w') as newfile:
     for line in oldfile:
         if not any(deps in line for deps in useless_deps):
             newfile.write(line)
