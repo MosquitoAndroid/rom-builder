902f7952f45c1b252febeb43dd18b366fe3ce65d
Witherking25 <witherking@withertech.com>
2023-01-18 23:41:21 -0500

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -12,8 +12,7 @@
 echo "***BuildBot***"
 echo $TELEGRAM_TOKEN > /tmp/tg_token
 echo $GH_PERSONAL_TOKEN > /tmp/gh_token
-echo $GH_REPO_NAME > /tmp/gh_repo
-echo $DRONE_BUILD_NUMBER > /tmp/build_no
+echo $BUILD_NUMBER > /tmp/build_no
 echo $TELEGRAM_CHAT > /tmp/tg_chat
 #echo `pwd` > /tmp/loc
 #sudo echo "ci ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
